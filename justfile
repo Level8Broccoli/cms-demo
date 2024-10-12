922db927de2ffca1abbd4f6e49a6301f9ad76eed
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2024-10-12 16:52:55 +0100

    Init vite

--- justfile
+++ justfile
@@ -1,11 +1,5 @@
 default:
     @just --list
 
-dynamic:
-    DENO_FUTURE=1 deno task dynamic
-
-static:
-    DENO_FUTURE=1 deno task static
-
 fmt:
-    DENO_FUTURE=1 deno fmt
+    deno fmt
--- static-generator/generator.ts
+++ static-generator/generator.ts
@@ -1,5 +1,7 @@
 import { renderToString } from "preact-render-to-string";
 import { App } from "../app/App.tsx";
 
-await Deno.mkdir("out", { recursive: true });
-await Deno.writeTextFile("out/static.html", renderToString(App()));
+const OUTDIR = "dist";
+
+await Deno.mkdir(OUTDIR, { recursive: true });
+await Deno.writeTextFile(`${OUTDIR}/static.html`, renderToString(App()));
