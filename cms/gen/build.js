a9b637d416e8021fd134f32f155e9ed872f5a826
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2024-10-12 18:00:25 +0100

    Remove generated files

--- cms/gen/build.js
+++
@@ -1,6 +0,0 @@
-import { render as o } from "preact";
-import { jsx as r } from "preact/jsx-runtime";
-function n() {
-  return console.log("this runs"), r("h1", { children: "CMS Test" });
-}
-o(n(), document.body);
